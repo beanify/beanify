61899f1aad9372e560ee4cfedefa72781794261e
WangJun <502554248@qq.com>
2020-08-12 18:34:22 +0800

    1.fix return null will not ajv check

--- plugins/ajv.js
+++ plugins/ajv.js
@@ -1,5 +1,5 @@
 const AJV = require("ajv")
-const errors=require("../errors")
+const errors = require("../errors")
 
 module.exports = (beanify, opts, done) => {
 
@@ -10,25 +10,25 @@ module.exports = (beanify, opts, done) => {
       removeAdditional: 'all'
     })
 
-    if(typeof schema.body === 'object'){
-      bodyCheck=ajv.compile(schema.body)
-      if(bodyCheck(request.$req.body)===false){
+    if (typeof schema.body === 'object') {
+      bodyCheck = ajv.compile(schema.body)
+      if (request.$req.body && bodyCheck(request.$req.body) === false) {
         const err = new errors.SchemaError(ajv.errorsText(bodyCheck.errors))
         throw err
       }
     }
   })
 
-  beanify.addHook('onAfterHandler',(request)=>{
+  beanify.addHook('onAfterHandler', (request) => {
     const schema = request.schema || {}
 
     const ajv = new AJV({
       removeAdditional: 'all'
     })
 
-    if(typeof schema.response === 'object'){
-      responseCheck=ajv.compile(schema.response)
-      if(responseCheck(request.$res.res)===false){
+    if (typeof schema.response === 'object') {
+      responseCheck = ajv.compile(schema.response)
+      if (request.$res.res && responseCheck(request.$res.res) === false) {
         const err = new errors.SchemaError(ajv.errorsText(responseCheck.errors))
         throw err
       }
