2ea3f827624aa85773910923a0b3980d677f77f8
445212619@qqcom <beanjs>
2020-12-18 16:02:34 +0800

    1.fixed export types

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,42 @@ import { Beanify, BeanifyOptions } from './types/beanify'
 declare function factory (options: BeanifyOptions): Beanify
 
 export = factory
+
+export {
+  BeanifyOptions,
+  BeanifyDoneCallback,
+  BeanifyAfter,
+  BeanifyReady,
+  BeanifyClose,
+  Beanify
+} from './types/beanify'
+
+export { ErrioOptions, Errio } from './types/errio'
+
+export {
+  HookDoneCallback,
+  OnCloseCallback,
+  OnErrorCallback
+} from './types/hooks'
+
+export {
+  PluginDoneCallback,
+  PluginOptions,
+  PluginCallback,
+  BeanifyPlugin
+} from './types/plugin'
+
+export {
+  RouterOptions,
+  Request,
+  Reply,
+  InjectHandler,
+  OnRouteCallback,
+  OnBeforeInject,
+  onAfterInject,
+  RouteHandler,
+  OnBeforeHandler,
+  OnAfterHandler,
+  Route,
+  Inject
+} from './types/router'
